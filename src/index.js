5b376a7b21c94b96bd7feb125202b38e98d4cc18
shest28 <shest28@gmail.com>
2018-02-22 15:45:06 +0300

    smart-calculator/fix: fix evaluate function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-const evaluate = require('./evaluate.js');
 class SmartCalculator {
+	
   constructor(initialValue) {
 	this.value = initialValue;
 	this.str = initialValue + '';
   }
   
  toString() {
+	function evaluate(arr) {
+	
+		evalSign(arr, '^');
+		evalSign(arr, '*'); 
+		evalSign(arr, '/');
+		evalSign(arr, '-');
+		evalSign(arr, '+');
+		return arr[0];
+	  
+	  function evalSign(arr, sign) {
+		  let index = arr.indexOf(sign);
+		  while(index > -1) {
+			switch(sign) {
+			  case '^': var val = Math.pow(arr[index - 1], arr[index + 1]); break;
+			  case '*': var val = arr[index - 1] * arr[index + 1]; break;
+			  case '/': var val = arr[index - 1] / arr[index + 1]; break;
+			  case '+': var val = parseInt(arr[index - 1]) + parseInt(arr[index + 1]); break;
+			  case '-': var val = parseInt(arr[index - 1]) - parseInt(arr[index + 1]); break;  
+			}
+			arr.splice(index - 1, 3, val); 
+			index = arr.indexOf(sign); 
+		   }
+	  } 
+	}
 	this.value = evaluate(this.str.match(/\d+|\+|-|\*|\/|\^/g));
     return this.value;
   }
